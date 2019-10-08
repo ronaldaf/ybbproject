1ced391585d3bb031da627b786e0e90c38d78b7b
hdpolover <hendrapolover@gmail.com>
2019-10-08 20:12:42 +0700

    up

--- app/src/main/java/com/hdpolover/ybbproject/TestActivity.java
+++ app/src/main/java/com/hdpolover/ybbproject/TestActivity.java
@@ -10,11 +10,5 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
-<<<<<<< Updated upstream
-        //KOMEN IVAL
-=======
-        //ini juga kpmen
->>>>>>> Stashed changes
     }
 }
