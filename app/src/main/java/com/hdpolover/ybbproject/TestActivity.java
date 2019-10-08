35259b32ff08740eeb745903674fead1fdbf29fb
Ronald Arrival Fajar <ronaldarrivalf@gmail.com>
2019-10-08 20:10:17 +0700

    ivaltesting

--- app/src/main/java/com/hdpolover/ybbproject/TestActivity.java
+++ app/src/main/java/com/hdpolover/ybbproject/TestActivity.java
@@ -10,5 +10,7 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        //KOMEN IVAL
     }
 }
