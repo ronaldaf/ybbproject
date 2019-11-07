e9428a47814cc99f3191f17c122f5dcd6298f4a3
hdpolover <hendrapolover@gmail.com>
2019-11-07 09:56:18 +0700

    updated post view on homefragment

--- app/src/main/java/com/hdpolover/ybbproject/HomeFragment.java
+++ app/src/main/java/com/hdpolover/ybbproject/HomeFragment.java
@@ -2,23 +2,50 @@ package com.hdpolover.ybbproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hdpolover.ybbproject.adapters.AdapterPost;
+import com.hdpolover.ybbproject.models.ModelPost;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     FirebaseAuth firebaseAuth;
+    FloatingActionButton fab;
+
+    RecyclerView recyclerView;
+    List<ModelPost> postList;
+    AdapterPost adapterPost;
+
+    public HomeFragment() {
+        //required empty constructor
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,10 +53,84 @@ public class HomeFragment extends Fragment {
 
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance();
+        fab = view.findViewById(R.id.fab);
+
+        //recycler view and its properties
+        recyclerView = view.findViewById(R.id.postRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        //show newest post first, for this load from last
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        //set layput to recycler view
+        recyclerView.setLayoutManager(layoutManager);
+
+        //init post list
+        postList = new ArrayList<>();
+
+        loadPosts();
 
         return view;
     }
 
+    private void loadPosts() {
+        //path of all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+        //get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    postList.add(modelPost);
+
+                    //adapter
+                    adapterPost = new AdapterPost(getActivity(), postList);
+                    //set adapter recycler view
+                    recyclerView.setAdapter(adapterPost);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //in case of error
+                Toast.makeText(getActivity(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void searchPosts(final String searchQuery) {
+        //path of all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+        //get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    if (modelPost.getpTitle().toLowerCase().contains(searchQuery.toLowerCase()) ||
+                    modelPost.getpDesc().toLowerCase().contains(searchQuery.toLowerCase())) {
+                        postList.add(modelPost);
+                    }
+
+                    //adapter
+                    adapterPost = new AdapterPost(getActivity(), postList);
+                    //set adapter recycler view
+                    recyclerView.setAdapter(adapterPost);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //in case of error
+                Toast.makeText(getActivity(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 
     private void checkUserStatus() {
         //get current user
@@ -58,6 +159,39 @@ public class HomeFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         //inflating menu
         inflater.inflate(R.menu.menu_main, menu);
+
+        //hide logout
+        menu.findItem(R.id.action_logout).setVisible(false);
+
+        //search view to search post by post title/desc
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)MenuItemCompat.getActionView(item);
+
+        //search listener
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //called when user press search button
+                if (!TextUtils.isEmpty(query)) {
+                    searchPosts(query);
+                } else {
+                    loadPosts();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //called as and when useer press any letter
+                if (!TextUtils.isEmpty(newText)) {
+                    searchPosts(newText);
+                } else {
+                    loadPosts();
+                }
+                return false;
+            }
+        });
+
         super.onCreateOptionsMenu(menu, inflater);
     }
 
---
+++ app/src/main/java/com/hdpolover/ybbproject/adapters/AdapterPost.java
@@ -0,0 +1,143 @@
+package com.hdpolover.ybbproject.adapters;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hdpolover.ybbproject.R;
+import com.hdpolover.ybbproject.models.ModelPost;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterPost extends RecyclerView.Adapter<AdapterPost.MyHolder> {
+
+    Context context;
+    List<ModelPost> postList;
+
+    public  AdapterPost(Context context, List<ModelPost> postList) {
+        this.context = context;
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //inflate layout row_post.xml
+        View view = LayoutInflater.from(context).inflate(R.layout.row_posts, viewGroup, false);
+
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder myHolder, int position) {
+        //get data
+        String uid = postList.get(position).getUid();
+        String uEmail = postList.get(position).getuEmail();
+        String uName = postList.get(position).getuName();
+        String uDp = postList.get(position).getuDp();
+        String pId = postList.get(position).getpId();
+        String pTitle = postList.get(position).getpTitle();
+        String pDesc = postList.get(position).getpDesc();
+        String pImage = postList.get(position).getpImage();
+        String pTimestamp = postList.get(position).getpTime();
+
+        //convert timestamp to dd/mm/yyy hh:mm am/pm
+        Calendar calendar = Calendar.getInstance(Locale.getDefault());
+        calendar.setTimeInMillis(Long.parseLong(pTimestamp));
+        String pTime = DateFormat.format("dd/MM/yyy hh:mm aa", calendar).toString();
+
+        //set data
+        myHolder.uNameTv.setText(uName);
+        myHolder.pTimeTv.setText(pTime);
+        myHolder.pTitleTv.setText(pTitle);
+        myHolder.pDescTv.setText(pDesc);
+
+        //set user dp
+        try {
+            Picasso.get().load(uDp).placeholder(R.drawable.ic_default_img).into(myHolder.uPictureIv);
+        } catch (Exception e) {
+
+        }
+
+        //set post image
+        //if no image then hide the imageview
+        if (pImage.equals("noImage")) {
+            //hide imageview
+            myHolder.pImageIv.setVisibility(View.GONE);
+        } else {
+            try {
+                Picasso.get().load(pImage).into(myHolder.pImageIv);
+            } catch (Exception e) {
+
+            }
+        }
+
+
+        //handle button clicks
+        myHolder.moreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //will implement later
+                Toast.makeText(context, "More", Toast.LENGTH_SHORT).show();
+            }
+        });
+        myHolder.upvoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //will implement later
+                Toast.makeText(context, "Upvote", Toast.LENGTH_SHORT).show();
+            }
+        });
+        myHolder.commentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //will implement later
+                Toast.makeText(context, "Comment", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    //view holder class
+    class MyHolder extends RecyclerView.ViewHolder {
+        //view from row_post.xml
+        ImageView uPictureIv, pImageIv;
+        TextView uNameTv, pTimeTv, pTitleTv, pDescTv, pUpvotesTv;
+        ImageButton moreBtn;
+        Button upvoteBtn, commentBtn;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //init views
+            uPictureIv = itemView.findViewById(R.id.uPictureIv);
+            pImageIv = itemView.findViewById(R.id.pImageIv);
+            uNameTv = itemView.findViewById(R.id.uNameTv);
+            pTitleTv = itemView.findViewById(R.id.pTitleTv);
+            pDescTv = itemView.findViewById(R.id.pDescTv);
+            pTimeTv = itemView.findViewById(R.id.pTimeTv);
+            pUpvotesTv = itemView.findViewById(R.id.pUpvoteTv);
+            moreBtn = itemView.findViewById(R.id.moreBtn);
+            upvoteBtn = itemView.findViewById(R.id.upvoteBtn);
+            commentBtn = itemView.findViewById(R.id.commentBtn);
+
+        }
+    }
+}
